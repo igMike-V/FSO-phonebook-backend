c9effb422928bf5d89a7ebbaceb0301f7bd584d4
Mike Vautour <mike@intergalacticlab.com>
2023-02-06 13:11:27 -0500

    added morgan middleware 3.7

--- index.js
+++ index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
 app.use(express.json())
+app.use(morgan('tiny'))
+
+// Morgan Middleware logging to console
+
+
 
 // Hardcoded Data
 let persons = [
