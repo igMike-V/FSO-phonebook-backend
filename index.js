6471cad76e1c56dfdfd1bf9bee7bd6501c4bdf0a
Mike Vautour <mike@intergalacticlab.com>
2023-02-09 10:09:17 -0500

    All but delete working and tested on Frontend

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
+require('dotenv').config()
 const express = require('express')
 const cors = require('cors')
 const morgan = require('morgan')
 const app = express()
+const Person = require('./models/person')
+const { response } = require('express')
 
 app.use(cors())
 
@@ -21,69 +24,40 @@ app.use(express.json())
 app.use(morgan(':method :url :status :response-time :req-data'))
 
 
-// Hardcoded Data
-let persons = [
-    { 
-      "id": 1,
-      "name": "Arto Hellas", 
-      "number": "040-123456"
-    },
-    { 
-      "id": 2,
-      "name": "Ada Lovelace", 
-      "number": "39-44-5323523"
-    },
-    { 
-      "id": 3,
-      "name": "Dan Abramov", 
-      "number": "12-43-234345"
-    },
-    { 
-      "id": 4,
-      "name": "Mary Poppendieck", 
-      "number": "39-23-6423122"
-    }
-]
-
 //Routes
-
 app.get('/', (req, res) => {
     res.send('<h1>Phonebook Api</h1><p>Use /api/persons to return the listings.</p>')
 })
 
-// Return info on the api
+// Return info on the api  
 app.get('/info', (req, res) => {
     const date = new Date()
-    const pageContent = `
-        <p>Phonebook has info for ${persons.length} ${persons.length === 1 ? 'person' : 'people' }</p>
-        <p>${date}</p>
-    `
-    res.send(pageContent)
+
+    Person.find({}).then(people => {
+        const pageContent = `
+            <p>Phonebook has info for ${people.length} ${people.length === 1 ? 'person' : 'people' }</p>
+            <p>${date}</p>
+        `
+        res.send(pageContent)
+    })
 })
 
 // Get all people
 app.get('/api/persons', (req, res) => {
-    res.json(persons)
+    Person.find({}).then(people => {
+        res.json(people)
+    })
 })
 
 // Get a single person
 app.get('/api/persons/:id', (req, res) => {
     // Id from params is a string.  convert to number
-    const id = Number(req.params.id)
-    const person = persons.find(item => item.id === id)
-    
-    // Check if person contains something
-    if(person){
-        // Respond with person
+    Person.findById(req.params.id).then(person => {
         res.json(person)
-    } else {
-        // Set status as not found and end
-        res.status(404).end()
-    }
-    
+    })
 })
 
-// Delete an entry
+// TODO Delete an entry
 app.delete('/api/persons/:id', (req, res) => {
     // Id from params is a string.  convert to number
     const id = Number(req.params.id)
@@ -95,28 +69,15 @@ app.delete('/api/persons/:id', (req, res) => {
     }
 })
 
-// Get a unique id for the post
-const getRandomId = () => {
-    let id = 0
-    // Set id to a random number, continue getting new random numbers until a unique value is found
-    do {
-        id = Math.floor(Math.random() * 99999999999)
-    } while (persons.find(person => person.id === id))
-    return id
-}
-
 // Add a new entry
 app.post('/api/persons', (req, res) => {
     const body = req.body
-    let newId = getRandomId()
-    
-    // Error handling for name
+     // Error handling for name
     if(!body.name){
         return res.status(400).json({
             error: 'Request must contain a name'
         })
     }
-
     // Error handling for number
     if(!body.number){
         return res.status(400).json({
@@ -124,13 +85,14 @@ app.post('/api/persons', (req, res) => {
         })
     }
 
-    const newEntry = { 
-        "id": newId,
+    const newPerson = new Person({ 
         "name": body.name, 
         "number": body.number
-      }
-    persons.push(newEntry)
-    res.json(newEntry)
+      })
+
+    newPerson.save().then(savedPerson => {
+        res.json(savedPerson)
+    })
 
 })
 
