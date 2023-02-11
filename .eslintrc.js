734c11af4dfccefa12dbaf43510bde84c05f3511
Mike Vautour <mike@intergalacticlab.com>
2023-02-11 15:41:21 -0500

    eslint rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,10 +4,24 @@ module.exports = {
     'es2021': true,
     'node': true
   },
-  'extends': 'eslint:recommended',
+  'extends': [
+    'eslint:recommended',
+    "plugin:import/errors", // if that file exports something the other file can import it and if it doesn't export it cant be imported somewhere else
+    "plugin:jsx-a11y/recommended", // this is doing some more abilities to understand accessibility
+  ],
   'parserOptions': {
-    'ecmaVersion': 'latest'
+    'ecmaVersion': 'latest',
+    "sourceType": "module",
+    "ecmaFeatures": {
+      "jsx": true
+    }
   },
+  "settings": {
+    "react": {
+      "version": "detect" // you have to tell eslint which version React are using, "detect" will say can you just figure out yourself (from package.json)
+    }
+  },
+  "plugins": ["react"],
   'rules': {
     'indent': [
       'error',
@@ -33,6 +47,10 @@ module.exports = {
     'arrow-spacing': [
       'error', { 'before': true, 'after': true }
     ],
-    'no-console': 0
+    'no-console': 0,
+    "react/prop-types": 0,
+    "react/jsx-uses-react": "error",   
+    "react/jsx-uses-vars": "error",
+     "no-unused-vars": 0
   }
 }
