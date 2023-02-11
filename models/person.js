65d251d3bdd680fc0eab6cd65def1d27684fef95
Mike Vautour <mike@intergalacticlab.com>
2023-02-11 09:48:15 -0500

    Linted

--- models/person.js
+++ models/person.js
@@ -4,36 +4,36 @@ mongoose.set('strictQuery', false)
 const url = process.env.MONGODB_URI
 
 mongoose.connect(url)
-    .then(result => {
-        console.log("connected to mongodb")
-    })
-    .catch((error) => {
-        console.log('error connecting to MongoDB:', error.message)
-    })
+  .then(() => {
+    console.log('connected to mongodb')
+  })
+  .catch((error) => {
+    console.log('error connecting to MongoDB:', error.message)
+  })
 
 const personSchema = new mongoose.Schema({
-    name: {
-        type: String,
-        minLength: 3,
-        required: true,
-        unique: true
+  name: {
+    type: String,
+    minLength: 3,
+    required: true,
+    unique: true
+  },
+  number: {
+    type: String,
+    validate: {
+      validator: value => /^\d{2,3}-\d{6,}/.test(value)
     },
-    number: {
-        type: String,
-        validate: {
-            validator: value => /^\d{2,3}\-\d{6,}/.test(value)
-        },
-        required: true
-    }
+    required: true
+  }
 })
 
 
 personSchema.set('toJSON', {
-    transform: (document, returnedObject) => {
-      returnedObject.id = returnedObject._id.toString()
-      delete returnedObject._id
-      delete returnedObject.__v
-    }
-  })  
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString()
+    delete returnedObject._id
+    delete returnedObject.__v
+  }
+})
 
-module.exports = mongoose.model("Person", personSchema)
+module.exports = mongoose.model('Person', personSchema)
